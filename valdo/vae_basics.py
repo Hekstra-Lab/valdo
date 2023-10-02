abd7e504a61922089730ad45e298d0f374f7c5fd
Doeke Hekstra <dhekstra@holy7c24101.rc.fas.harvard.edu>
2023-10-01 21:48:50 -0400

    Revert "Merge branch 'experimental' of https://github.com/Hekstra-Lab/drug-screening into experimental"
    This reverts commit eb71ea280df9978d8650fbaee9e772a2982fde3e, reversing
    changes made to 79befa028ea5436533701f92e122f7791ba24534.
    - which is what we want to do

--- valdo/vae_basics.py
+++ valdo/vae_basics.py
@@ -116,17 +116,14 @@ class DenseNet(nn.Module):
     def forward(self, x):
         return self._layers(x)
 
-def sampling(z_mean, z_log_var, repeats=None):
+def sampling(z_mean, z_log_var):
     """
     Reparameterization Trick
     Uses (z_mean, z_log_var) to sample z, with normal distribution.
     """
     batch = z_mean.size(0)
     dim = z_mean.size(1)
-    if repeats is None:
-        epsilon = torch.randn(batch, dim, device=z_mean.device)
-    else:
-        epsilon = torch.randn(repeats, batch, dim, device=z_mean.device)
+    epsilon = torch.randn(batch, dim, device=z_mean.device)
     return z_mean + torch.exp(0.5 * z_log_var) * epsilon
 
 
